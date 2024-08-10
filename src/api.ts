491690758633c90873c3dc260dff8797ea2f1a0f
Isaac Leonard <isaac.mjs.leonard@gmail.com>
2024-08-11 00:05:19 +1000

    Minor changes to improve typing

--- src/api.ts
+++ src/api.ts
@@ -5,19 +5,23 @@ import { commands } from "./bindings";
 export const state = signal(await commands.getAppInfo());
 type Api = typeof commands;
 type ExtractData<Api> = {
-  [K in keyof Api]: Api[K] extends Promise<Result<infer Data, infer Err>>
-    ? Promise<Data>
+  [K in keyof Api]: Api[K] extends (
+    ...args: infer Args
+  ) => Promise<Result<infer Data, infer _Err>>
+    ? (...args: Args) => Promise<Data>
     : Api[K];
 };
 
 export const client: ExtractData<Api> = Object.entries(commands).reduce(
   (client, [key, fn]) => {
-    const func = async <T extends []>(...args: T) => {
-      const res = await fn(...args);
-      if (res?.status === "ok") {
-        return res.data;
-      } else if (res?.status === "error") {
-        throw res.error;
+    const func = async (...args: Parameters<typeof fn>) => {
+      const res = await (fn as any)(...args);
+      if (typeof res === "object" && res !== null && "status" in res) {
+        if (res.status === "ok") {
+          return res.data;
+        } else if (res?.status === "error") {
+          throw res.error;
+        }
       } else {
         return res;
       }
@@ -35,7 +39,7 @@ export const client: ExtractData<Api> = Object.entries(commands).reduce(
       return { ...client, [key]: func };
     }
   },
-  {}
+  {} as ExtractData<Api>
 );
 
 export const rerender = () => {
