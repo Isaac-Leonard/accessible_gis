14e4914b29bdd7bac1b9b545a9a57c98ca8276a4
Isaac Leonard <isaac.mjs.leonard@gmail.com>
2024-08-12 22:26:51 +1000

    Refactoring

---
+++ src/classification.tsx
@@ -0,0 +1,118 @@
+import { useState } from "preact/hooks";
+import { client } from "./api";
+import { Classification } from "./bindings";
+import { SaveButton } from "./save-button";
+import { Dialog, useDialog } from "./dialog";
+
+export const ClassificationDialog = () => {
+  const { open, innerRef, setOpen } = useDialog();
+
+  return (
+    <Dialog
+      modal={true}
+      open={open}
+      setOpen={setOpen}
+      openText="Classify raster"
+    >
+      <ClassificationScreen onClassify={() => setOpen(false)} />
+    </Dialog>
+  );
+};
+
+type StringClassification = { target: string; min: string; max: string };
+
+const ClassificationScreen = ({ onClassify }: { onClassify: () => void }) => {
+  const [classifications, setClassifications] = useState<
+    StringClassification[]
+  >([]);
+  console.log(classifications);
+  const handleInput =
+    (index: number, key: keyof Classification) => (value: string) => {
+      const newClassifications = [...classifications];
+      newClassifications[index] = {
+        ...newClassifications[index],
+        [key]: Number(value),
+      };
+      setClassifications(newClassifications);
+    };
+
+  return (
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th>Class</th> <th>Target</th>
+            <th>Min</th>
+            <th>Max</th>
+          </tr>
+        </thead>
+        <tbody>
+          {" "}
+          {classifications.map(({ target, min, max }, i) => (
+            <tr>
+              <td>{i}</td>
+              <td>
+                <ClassificationInput
+                  value={target}
+                  setValue={handleInput(i, "target")}
+                />
+              </td>
+              <td>
+                <ClassificationInput
+                  value={min}
+                  setValue={handleInput(i, "min")}
+                />
+              </td>
+              <td>
+                <ClassificationInput
+                  value={max}
+                  setValue={handleInput(i, "max")}
+                />
+              </td>
+            </tr>
+          ))}
+        </tbody>{" "}
+      </table>
+      <button
+        onClick={() =>
+          setClassifications([
+            ...classifications,
+            { target: "", min: "", max: "" },
+          ])
+        }
+      >
+        Add Classification
+      </button>
+      <SaveButton
+        text="Save classified raster"
+        prompt="Classified raster destination"
+        onSave={async (file) => {
+          await client.classifyCurrentRaster(
+            file,
+            classifications.map(mapClassification)
+          );
+          onClassify();
+        }}
+      />
+    </div>
+  );
+};
+
+type ClassificationInputProps = {
+  value: string;
+  setValue: (value: string) => void;
+};
+
+const ClassificationInput = ({ value, setValue }: ClassificationInputProps) => {
+  return (
+    <input value={value} onInput={(e) => setValue(e.currentTarget.value)} />
+  );
+};
+
+const mapClassification = (
+  classification: StringClassification
+): Classification => ({
+  target: Number(classification.target),
+  min: Number(classification.min),
+  max: Number(classification.max),
+});
