fb87cf4edcc903b9e97de87b82a1c88d776c8d4e
Isaac Leonard <isaac.mjs.leonard@gmail.com>
2024-08-19 12:17:48 +1000

    Add the ability to provide extention filters to the SaveButton component

--- src/save-button.tsx
+++ src/save-button.tsx
@@ -1,17 +1,23 @@
-import { save } from "@tauri-apps/plugin-dialog";
+import { DialogFilter, save } from "@tauri-apps/plugin-dialog";
 
 type SaveButtonProps = {
   text?: string;
   onSave: (name: string) => void;
   prompt?: string;
+  filters?: DialogFilter[];
 };
 
-export const SaveButton = ({ onSave, text, prompt }: SaveButtonProps) => {
+export const SaveButton = ({
+  onSave,
+  text,
+  prompt,
+  filters,
+}: SaveButtonProps) => {
   if (typeof text !== "string") {
     text = "Save";
   }
   const clickHandler = async () => {
-    const name = await save({ title: prompt });
+    const name = await save({ title: prompt, filters });
     if (name !== null) {
       onSave(name);
     }
