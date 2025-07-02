66d2d2a7c53d31660abae9f0bcf9f5f5e826825d
Isaac Leonard <isaac.mjs.leonard@gmail.com>
2025-07-02 15:32:04 +1000

    Add error to desktop application code for failure to run external programs

--- desktop/src-tauri/src/tools/mod.rs
+++ desktop/src-tauri/src/tools/mod.rs
@@ -13,6 +13,7 @@ pub enum DemClassificationError {
     Polygonise(String),
     MajorityFilter(String),
     LabelLandForms(String),
+    FailToRun(String),
 }
 
 pub fn dem_to_landform_polygons(
@@ -27,27 +28,28 @@ pub fn dem_to_landform_polygons(
     let path = app.path();
     let geomorphons_output = path
         .resolve("output_geomorphons.tif", BaseDirectory::Temp)
-        .unwrap();
+        .expect("Could not find temp directory");
     proc_to_result(geomorphons(
         input,
         &geomorphons_output,
         search,
         threshold,
         distance,
-    ))
+    )?)
     .map_err(DemClassificationError::Geomorphons)?;
     let majority_filter_output = path
         .resolve("output_majority_filter.tif", BaseDirectory::Temp)
-        .unwrap();
+        .expect("Could not find temp directory");
     proc_to_result(majority_filter(
         &geomorphons_output,
         &majority_filter_output,
         filter,
-    ))
+    )?)
     .map_err(DemClassificationError::MajorityFilter)?;
-    proc_to_result(polygonise(&majority_filter_output, output.as_ref()))
+    proc_to_result(polygonise(&majority_filter_output, output.as_ref())?)
         .map_err(DemClassificationError::Polygonise)?;
-    proc_to_result(label_landforms(output, app)).map_err(DemClassificationError::LabelLandForms)?;
+    proc_to_result(label_landforms(output, app)?)
+        .map_err(DemClassificationError::LabelLandForms)?;
     Ok(())
 }
 
@@ -57,7 +59,7 @@ fn geomorphons(
     search: usize,
     threshold: f64,
     distance: usize,
-) -> Output {
+) -> Result<Output, DemClassificationError> {
     let mut command = wbt();
     command.args(["-r", "Geomorphons", "-v"]);
     command.arg("--dem").arg(input.as_ref());
@@ -69,25 +71,35 @@ fn geomorphons(
     command.arg("--tdist").arg(distance.to_string().as_str());
     command.arg("--forms");
     command.arg("--cd").arg("./");
-    command.output().unwrap()
+    run_program(command)
 }
 
-fn majority_filter(input: impl AsRef<Path>, output: impl AsRef<Path>, filter: usize) -> Output {
+fn majority_filter(
+    input: impl AsRef<Path>,
+    output: impl AsRef<Path>,
+    filter: usize,
+) -> Result<Output, DemClassificationError> {
     let mut command = wbt();
     command.args(["-r", "MajorityFilter"]);
     command.arg("-i").arg(input.as_ref());
     command.arg("-o").arg(output.as_ref());
     command.arg("--filter").arg(filter.to_string().as_str());
-    command.output().unwrap()
+    run_program(command)
 }
 
-fn polygonise(input: impl AsRef<Path>, output: impl AsRef<Path>) -> Output {
+fn polygonise(
+    input: impl AsRef<Path>,
+    output: impl AsRef<Path>,
+) -> Result<Output, DemClassificationError> {
     let mut command = Command::new("gdal_polygonize");
     command.arg(input.as_ref()).arg(output.as_ref());
-    command.output().unwrap()
+    run_program(command)
 }
 
-fn label_landforms(input: impl AsRef<Path>, app: AppHandle) -> Output {
+fn label_landforms(
+    input: impl AsRef<Path>,
+    app: AppHandle,
+) -> Result<Output, DemClassificationError> {
     let path = app.path();
     let mut command = Command::new("python3.13");
     command
@@ -97,7 +109,7 @@ fn label_landforms(input: impl AsRef<Path>, app: AppHandle) -> Output {
         )
         .arg(input.as_ref())
         .arg("dn");
-    command.output().unwrap()
+    run_program(command)
 }
 
 fn wbt() -> Command {
@@ -110,4 +122,10 @@ fn proc_to_result(output: Output) -> Result<String, String> {
     } else {
         Ok(String::from_utf8_lossy(&output.stdout).to_string())
     }
+}
+
+fn run_program(mut command: Command) -> Result<Output, DemClassificationError> {
+    command.output().map_err(|_| {
+        DemClassificationError::FailToRun(command.get_program().to_string_lossy().to_string())
+    })
 }
