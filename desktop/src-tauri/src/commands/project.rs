adda21a5ef3b69bca16312db4aa2238e3eba7324
Isaac Leonard <isaac.mjs.leonard@gmail.com>
2025-07-20 17:35:10 +1000

    Add the project.rs file for commands in the desktop application, should've been added earlier

---
+++ desktop/src-tauri/src/commands/project.rs
@@ -0,0 +1,21 @@
+use std::path::PathBuf;
+
+use crate::state::{AppState, projects::Project};
+
+#[tauri::command]
+#[specta::specta]
+pub fn create_project(path: PathBuf, state: AppState) {
+    state.with_lock(|state| match Project::new(path, state.settings()) {
+        Ok(project) => state.project = Some(project),
+        Err(err) => state.errors.push(err),
+    })
+}
+
+#[tauri::command]
+#[specta::specta]
+pub fn load_project(path: PathBuf, state: AppState) {
+    state.with_lock(|state| match Project::load(path) {
+        Ok(project) => state.project = Some(project),
+        Err(err) => state.errors.push(err),
+    })
+}
