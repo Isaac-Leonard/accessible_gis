cfbcd0267a9c4077362f80b7a4bc5a528ab4a7a3
Isaac Leonard <isaac.mjs.leonard@gmail.com>
2025-08-19 12:48:35 +1000

    Flip order of rendering vectors and rasters so vectors can be overlaied on top for the touch device

--- desktop/external-touch-device/src/main.ts
+++ desktop/external-touch-device/src/main.ts
@@ -322,8 +322,8 @@ class GisManager {
     this.ctx.fillStyle = "#000000";
     this.ctx.strokeStyle = "#ffffff";
     this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
-    this.vectorManager.render();
     this.renderRaster();
+    this.vectorManager.render();
   }
 }
 
